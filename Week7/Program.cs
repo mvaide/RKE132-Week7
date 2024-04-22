5a7e8a48364fb3de0b03abde62f90563d79727ec
Morris Vaide <mvaide@tktk.ee>
2024-04-22 14:46:02 +0300

    Week7

---
+++ Week7/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out)");
+string userChoice = Console.ReadLine(); 
+
+if (userChoice == "in")
+{
+PrintHello();
+}
+
+else
+{
+PrintGoodbye();
+}
+
+
+
+static void PrintHello()
+{
+ Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("Goodbye.");
+}
